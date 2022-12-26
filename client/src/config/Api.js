6bb453f6b5ffcabd2c0dccba40d0872356dbd87b
Faeq <faequl96@gmail.com>
2022-12-27 06:10:53 +0700

    fix env react baseUrl

--- client/src/config/Api.js
+++ client/src/config/Api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// "http://waysbucks-by-faequl.up.railway.app/api/v1"
+// "https://waysbucks-by-faequl.up.railway.app/api/v1"
 // "http://localhost:5000/api/v1"
 
 export const API = axios.create({
-   baseURL: "process.env.REACT_APP_BASEURL",
+   baseURL: process.env.REACT_APP_BASEURL,
 });
 
 export const setAuthToken = (token) => {
